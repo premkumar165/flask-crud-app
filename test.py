3fc7d0d7367ae5df5f07e166baedc798cf40c2fe
Prem <premkumar.mech08@gmail.com>
2019-12-06 15:05:48 -0500

    Test Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+    cmd = ["ls","-l"]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    return out
+if __name__ == "__main__" :
+    app.run()
